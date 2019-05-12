a9a62b2920b2806154549724abac1fd16c7d559b
Marcus Jonsson <marcus.m.jonsson@knowit.se>
2019-05-12 20:42:47 +0200

    Day one complete

---
+++ src/One.kt
@@ -0,0 +1,30 @@
+import java.io.File
+import java.lang.Integer.parseInt
+import java.util.stream.Collectors
+
+fun main(args: Array<String>) {
+    val fileName = "C:\\Users\\marjon4\\IdeaProjects\\AdventOfCode\\resources\\DayOne.txt"
+    
+    val integers = File(fileName).readLines().stream()
+        .map { line -> parseInt(line) }
+        .collect(Collectors.toList())
+    val seqRepeat = findFirstFreqRepeat(integers)
+    println("First found was $seqRepeat")
+}
+
+fun findFirstFreqRepeat(integers: List<Int>) : Int {
+    var setOfUsedFreqs = HashSet<Int>()
+    var freq = 0
+
+    while (true) {
+        integers.forEach {
+            freq += it
+            if (setOfUsedFreqs.contains(freq)) {
+                println("Reached $freq")
+                return freq
+            } else {
+                setOfUsedFreqs.add(freq);
+            }
+        }
+    }
+}
